2d11918c54f81a0848e54d5f2087b61e09d6a9be
jesuisunjerome <38988287+jesuisunjerome@users.noreply.github.com>
2020-07-13 22:12:56 -0500

    Commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$("document").ready(function () {
+    mostrarBarra();
+});
 
+
+function mostrarBarra() {
+    $("#btnSearch").click(function () {
+        $(".buscadorPrincipal").toggleClass("d-none");
+        $("#baChache .navbar-nav").toggleClass("d-none");
+    });
+
+    $("#btnCloseBaChache").click(function () {
+        $(".buscadorPrincipal").toggleClass("d-none");
+        $("#baChache .navbar-nav").toggleClass("d-none");
+    });
+
+    $("#lbtnFollowers").click(function () {
+        $("#rightSidebar").toggleClass("showMobile");
+    });
+}
